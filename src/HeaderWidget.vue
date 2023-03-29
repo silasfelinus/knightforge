47aac0242f73ff9e762bc2483192201d8b52a1c3
silasfelinus <silasfelinus@gmail.com>
2023-03-29 00:51:40 -0700

    bug hunting

---
+++ src/HeaderWidget.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-header>
+    <q-toolbar>
+      <q-btn flat dense round icon="menu" @click="$emit('toggle-sidebar')" />
+      <q-toolbar-title>KnightForge Wonderlab</q-toolbar-title>
+    </q-toolbar>
+  </q-header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HeaderWidget',
+});
+</script>
+
+<style scoped>
+@import './css/quasar.variables.scss';
+
+.q-header {
+  background-color: $secondary;
+  color: $white;
+}
+
+.q-toolbar-title {
+  font-weight: bold;
+}
+</style>
