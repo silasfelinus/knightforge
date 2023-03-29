e398cb4b4e7bef7ea667538fbada29880b48ad27
silasfelinus <silasfelinus@gmail.com>
2023-03-29 13:17:29 -0700

    bug hunting

--- src/App.vue
+++ src/App.vue
@@ -5,25 +5,27 @@
         <HeaderWidget />
       </q-header>
 
-      <q-drawer side="right" v-model="collapsedRight1" bordered>
-        <SidebarWidget side="right" preset="preset1" />
-      </q-drawer>
+      <q-layout>
+        <q-drawer side="left" v-model="collapsedLeft1" bordered>
+          <SidebarWidget side="left" preset="preset3" />
+        </q-drawer>
 
-      <q-drawer side="right" v-model="collapsedRight2" bordered>
-        <SidebarWidget side="right" preset="preset2" />
-      </q-drawer>
+        <q-drawer side="left" v-model="collapsedLeft2" bordered>
+          <SidebarWidget side="left" preset="preset4" />
+        </q-drawer>
 
-      <q-drawer side="left" v-model="collapsedLeft1" bordered>
-        <SidebarWidget side="left" preset="preset3" />
-      </q-drawer>
+        <q-page-container>
+          <MainWidget />
+        </q-page-container>
 
-      <q-drawer side="left" v-model="collapsedLeft2" bordered>
-        <SidebarWidget side="left" preset="preset4" />
-      </q-drawer>
+        <q-drawer side="right" v-model="collapsedRight1" bordered>
+          <SidebarWidget side="right" preset="preset1" />
+        </q-drawer>
 
-      <q-page-container>
-        <MainWidget />
-      </q-page-container>
+        <q-drawer side="right" v-model="collapsedRight2" bordered>
+          <SidebarWidget side="right" preset="preset2" />
+        </q-drawer>
+      </q-layout>
 
       <q-footer>
         <FooterWidget />
@@ -47,13 +49,32 @@ export default defineComponent({
     MainWidget,
     FooterWidget,
   },
-  setup() {
+  setup(_, { emit }) {
     const collapsedRight1 = ref(true);
     const collapsedRight2 = ref(true);
     const collapsedLeft1 = ref(true);
     const collapsedLeft2 = ref(true);
 
-    return { collapsedRight1, collapsedRight2, collapsedLeft1, collapsedLeft2 };
+    function toggleSidebar(side: string) {
+      if (side === 'right1') {
+        collapsedRight1.value = !collapsedRight1.value;
+      } else if (side === 'right2') {
+        collapsedRight2.value = !collapsedRight2.value;
+      } else if (side === 'left1') {
+        collapsedLeft1.value = !collapsedLeft1.value;
+      } else if (side === 'left2') {
+        collapsedLeft2.value = !collapsedLeft2.value;
+      }
+      emit('toggleSidebar', side);
+    }
+
+    return {
+      collapsedRight1,
+      collapsedRight2,
+      collapsedLeft1,
+      collapsedLeft2,
+      toggleSidebar,
+    };
   },
 });
 </script>
