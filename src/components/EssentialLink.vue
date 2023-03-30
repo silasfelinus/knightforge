306ae28703622614cb8a918af5a8f658d292e6b7
silasfelinus <silasfelinus@gmail.com>
2023-03-30 11:44:09 -0700

    replaced defaults

--- src/components/EssentialLink.vue
+++ src/components/EssentialLink.vue
@@ -13,43 +13,22 @@
     </q-item-section>
 
     <q-item-section>
-      <q-item-label>\{{ title }}</q-item-label>
-      <q-item-label caption>
-        \{{ caption }}
-      </q-item-label>
+      <q-item-label>{{ title }}</q-item-label>
+      <q-item-label caption>{{ caption }}</q-item-label>
     </q-item-section>
   </q-item>
 </template>
 
-<script{{#preset.typescript}} lang="ts"{{/preset.typescript}}>
-{{#if_eq typescriptConfig "class"}}import { Vue, prop, Options } from 'vue-class-component';
-class Props {
-  readonly title!: string;
-  readonly caption = prop({ default: '' });
-  readonly link = prop({ default: '#' });
-  readonly icon = prop({ default: '' });
+<script setup lang="ts">
+export interface EssentialLinkProps {
+  title: string;
+  caption?: string;
+  link?: string;
+  icon?: string;
 }
-@Options({})
-export default class EssentialLink extends Vue.with(Props) {}{{else}}import { defineComponent } from 'vue'
-export default defineComponent({
-  name: 'EssentialLink',
-  props: {
-    title: {
-      type: String,
-      required: true
-    },
-    caption: {
-      type: String,
-      default: ''
-    },
-    link: {
-      type: String,
-      default: '#'
-    },
-    icon: {
-      type: String,
-      default: ''
-    }
-  }
-}){{/if_eq}}
+withDefaults(defineProps<EssentialLinkProps>(), {
+  caption: '',
+  link: '#',
+  icon: '',
+});
 </script>
