78faaec38927d3dfddd3481f0565efe4116d89db
silasfelinus <silasfelinus@gmail.com>
2023-04-02 09:15:24 -0700

    dynamically loading presets

---
+++ src/pages/SplashScreen.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="splash-screen">
+    <img :src="randomImage" alt="Splash Screen" />
+  </div>
+</template>
+
+<script>
+import secretImages from '@/assets/splash/secretImages.json';
+import splashImages from '@/assets/splash/splashImages.json';
+
+export default {
+  name: 'SplashScreen',
+  data() {
+    return {
+      randomImage: '',
+    };
+  },
+  created() {
+    this.loadRandomImage();
+  },
+  methods: {
+    loadRandomImage() {
+      const allImages = [...secretImages, ...splashImages];
+      const randomIndex = Math.floor(Math.random() * allImages.length);
+      const selectedImage = allImages[randomIndex];
+      this.randomImage = require('@/assets/splash/' + selectedImage);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.splash-screen {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+}
+
+img {
+  max-width: 100%;
+  max-height: 100%;
+  object-fit: contain;
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import ErrorNotFound from '@/pages/ErrorNotFound.vue';
-import AppSimple from '@/pages/AppSimple.vue';
-import AppWorking from '@/pages/AppWorking.vue';
-import AppFull from '@/pages/AppFull.vue';
 import LoginForm from '@/pages/LoginForm.vue';
-import UnderConstruction from '@/pages/UnderConstruction.vue';
-import WarningScreen from '@/pages/WarningScreen.vue';
 
-const PRESET = 'AppSimple'; // Change this to switch between presets
+const PRESET = 'SplashScreen'; // Change this to switch between presets
+
+// Presets list (for reference)
+// AppSimple
+// AppWorking
+// AppFull
+// UnderConstruction
+// WarningScreen
+// SplashScreen
 
 const routes = [
   {
     path: '/',
-    component: {
-      AppSimple,
-      AppWorking,
-      AppFull,
-      UnderConstruction,
-      WarningScreen,
-    }[PRESET],
+    component: () =>
+      import(/* @vite-ignore */ `@/pages/${PRESET}.vue`).catch(() => {
+        console.error(`Failed to load preset: ${PRESET}`);
+        return ErrorNotFound;
+      }),
   },
   {
     path: '/login',
