af796ef10736786eab98ed69ff6e8d9a8fe3b0d5
silasfelinus <silasfelinus@gmail.com>
2023-04-03 00:32:34 -0700

    excluded tests from ts

--- src/tests/unit/App.spec.ts
+++ src/tests/unit/App.spec.ts
@@ -1,7 +1,7 @@
 // src/tests/unit/App.spec.ts
 
 import { shallowMount } from '@vue/test-utils';
-import App from '@/App.vue';
+import App from './App.vue';
 
 describe('App.vue', () => {
   it('renders the app successfully', () => {
