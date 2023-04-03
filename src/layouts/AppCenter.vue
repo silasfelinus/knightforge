e5e274b47b08afbb25776b29ee93652da9be794a
silasfelinus <silasfelinus@gmail.com>
2023-04-02 20:05:22 -0700

    added appcenter for mainscreen

---
+++ src/layouts/AppCenter.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page-container>
+    <router-view />
+  </q-page-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'AppCenter',
+});
+</script>
