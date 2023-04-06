999805e768d91191eb022982702d7ab9dbf71479
silasfelinus <silasfelinus@gmail.com>
2023-04-06 14:06:32 -0700

    redone design, bug hunting

--- src/layout/MagicScreen.vue
+++ src/layout/MagicScreen.vue
@@ -1,36 +1,63 @@
-<script>
-import draggable from 'vuedraggable';
+<template>
+  <div class="q-pa-md">
+    <q-draggable
+      :prevent-scroll="true"
+      :scroll-target="scrollContainer"
+      @drag="onDrag"
+    >
+      <q-resizable
+        :prevent-scroll="true"
+        :min-width="50"
+        :min-height="50"
+        @resize="onResize"
+      >
+        <div
+          ref="magicScreen"
+          class="magic-screen bg-primary text-white text-center q-gutter-md"
+          :style="styleObject"
+        >
+          <p>Magic Screen</p>
+        </div>
+      </q-resizable>
+    </q-draggable>
+  </div>
+</template>
 
+<script>
 export default {
-  components: {
-    draggable,
-  },
-  props: {
-    screenId: {
-      type: String,
-      required: true,
-    },
-  },
   data() {
     return {
-      screens: [], // To store your screens
+      scrollContainer: null,
+      styleObject: {
+        width: '200px',
+        height: '100px',
+      },
     };
   },
+  mounted() {
+    this.scrollContainer = this.$refs.magicScreen.parentElement.parentElement;
+  },
   methods: {
-    onScreenDropped() {
-      this.$emit('screenDropped', this.screenId);
+    onDrag(_, event) {
+      this.styleObject.left = event.left + 'px';
+      this.styleObject.top = event.top + 'px';
+    },
+    onResize(_, event) {
+      this.styleObject.width = event.width + 'px';
+      this.styleObject.height = event.height + 'px';
     },
   },
 };
 </script>
 
-<style lang="scss">
+<style scoped>
 .magic-screen {
-  position: absolute;
-  /* Add any other desired styles */
-}
-
-.draggable-area {
-  background-color: $accent;
+  border: 2px solid white;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  cursor: grab;
 }
 </style>
