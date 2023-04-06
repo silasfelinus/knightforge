5970fbfd7663ac209f2938ee8d6f5ed985af5399
silasfelinus <silasfelinus@gmail.com>
2023-04-05 18:01:54 -0700

    butterfly begin

---
+++ src/components/HolographicButterfly.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="holographic-butterfly">
+    <!-- Create a canvas for drawing the butterfly -->
+    <canvas ref="canvas" @mousemove="onMouseMove"></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HolographicButterfly',
+  data() {
+    return {
+      butterfly: null,
+      mouseX: 0,
+      mouseY: 0,
+    };
+  },
+  mounted() {
+    // Initialize the canvas and create the butterfly
+    this.initCanvas();
+    this.createButterfly();
+  },
+  methods: {
+    initCanvas() {
+      const canvas = this.$refs.canvas;
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+    },
+    createButterfly() {
+      // Create a butterfly with holographic and rainbow effects using math functions
+      // See the `createHolographicButterfly` function below
+    },
+    onMouseMove(event) {
+      // Update the mouse coordinates when the mouse moves
+      this.mouseX = event.clientX;
+      this.mouseY = event.clientY;
+    },
+    draw() {
+      // Draw the holographic butterfly on the canvas
+      // See the `draw` function below
+    },
+  },
+};
+</script>
+
+<style scoped>
+#holographic-butterfly {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
