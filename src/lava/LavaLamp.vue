49ba08a1f6d7a3bd9e91a49deebc4e1d35524d6e
silasfelinus <silasfelinus@gmail.com>
2023-04-07 22:54:27 -0700

    lava lamp

---
+++ src/lava/LavaLamp.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="lava-lamp">
+    <!-- The other elements like lava lamp container and bubbles will be added here later -->
+  </div>
+</template>
+
+<script lang="ts">
+// Import other components here later, e.g., import LavaBubble from './LavaBubble.vue';
+
+export default {
+  // Register other components here later, e.g., components: { LavaBubble },
+};
+</script>
+
+<style scoped>
+.lava-lamp {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background: radial-gradient(circle at center, #1a1a1a, #000000);
+}
+</style>
