a341b766fd86f2152f47787aa804b2eba6824cdd
silasfelinus <silasfelinus@gmail.com>
2023-04-11 15:23:04 -0700

    added ActiveComponents

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,64 +1,11 @@
 // src/router/index.ts
-import {
-  createRouter,
-  createWebHistory,
-  Router,
-  RouteRecordRaw,
-} from 'vue-router';
-import { Project } from '@/stores/types';
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import { projects } from '@/stores/projects';
+
+import { addActiveProjectsToRouter } from '@/stores/ActiveComponents';
 
 import ErrorScreen from '@/components/layout/ErrorScreen.vue';
 import HomePage from '@/components/layout/HomePage.vue';
-
-function getActiveProjects(projects: Project[]): Project[] {
-  return projects.filter((project) => project.isActive);
-}
-
-function createRoute(project: Project, componentName: string): RouteRecordRaw {
-  return {
-    path: `/${project.name.toLowerCase()}/${componentName.toLowerCase()}`,
-    name: `${project.name}-${componentName}`,
-    meta: {
-      isActive: project.isActive,
-    },
-    component: () =>
-      import(
-        /* @vite-ignore */
-        `@/components/${project.name.toLowerCase()}/${componentName}.vue`
-      ).catch(() => {
-        console.error(
-          `Error loading component for project: ${project.name}, component: ${componentName}`
-        );
-        return Promise.reject(new Error('Failed to load component'));
-      }),
-  };
-}
-
-function createRoutes(activeProjects: Project[]): RouteRecordRaw[] {
-  const routes: RouteRecordRaw[] = [];
-
-  activeProjects.forEach((project) => {
-    project.components.forEach((componentName) => {
-      const route = createRoute(project, componentName);
-      routes.push(route);
-    });
-  });
-
-  return routes;
-}
-
-function addRoutesToRouter(router: Router, routes: RouteRecordRaw[]) {
-  routes.forEach((route: RouteRecordRaw) => {
-    router.addRoute(route);
-  });
-}
-
-function addActiveProjectsToRouter(projects: Project[], router: Router) {
-  const activeProjects = getActiveProjects(projects);
-  const routes: RouteRecordRaw[] = createRoutes(activeProjects);
-  addRoutesToRouter(router, routes);
-}
 
 // Add the HomePage route and the ErrorScreen route as the last route in the array
 const routes: RouteRecordRaw[] = [
