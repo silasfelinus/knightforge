cd18ed9b6d6a3622859b80edf64ac1e642429174
silasfelinus <silasfelinus@gmail.com>
2023-04-12 11:11:45 -0700

    scaffolding

--- src/components/layout/NavigationMenu.vue
+++ src/components/layout/NavigationMenu.vue
@@ -1,33 +1,73 @@
 <template>
   <nav>
-    <ul>
-      <li v-for="route in activeRoutes" :key="route.name">
-        <router-link :to="{ name: route.name }" active-class="active">{{
-          route.name
-        }}</router-link>
+    <select v-model="selectedProject" @change="updateActiveComponents">
+      <option
+        v-for="project in projects"
+        :key="project.name"
+        :value="project.name"
+        :disabled="!project.isActive"
+      >
+        {{ project.name }}
+      </option>
+    </select>
+    <ul v-if="activeComponents.length > 0">
+      <li v-for="component in activeComponents" :key="component.alias">
+        <router-link :to="{ name: component.alias }" active-class="active">
+          {{ component.componentName }}
+        </router-link>
       </li>
     </ul>
   </nav>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import router from '@/router/index';
+import { defineComponent, ref, computed } from 'vue';
 import { components } from '@/stores/componentsGenerator';
+interface Project {
+  name: string;
+  isActive: boolean;
+}
+
+interface Component {
+  projectName: string;
+  componentName: string;
+  fileName: string;
+  isActive: boolean;
+  path: string;
+  alias: string;
+  importPath: string;
+}
 
 export default defineComponent({
   name: 'NavigationMenu',
   setup() {
-    const activeRoutes = router.options.routes.filter(
-      (route) =>
-        route.name &&
-        components.some(
-          (component) => component.alias === route.name && component.isActive
-        )
-    );
+    const selectedProject = ref('');
+    const projects = computed((): Project[] => {
+      return components.reduce((acc: Project[], component: Component) => {
+        if (!acc.some((project) => project.name === component.projectName)) {
+          acc.push({
+            name: component.projectName,
+            isActive: component.isActive,
+          });
+        }
+        return acc;
+      }, []);
+    });
+
+    const activeComponents = ref<Component[]>([]);
+
+    const updateActiveComponents = () => {
+      activeComponents.value = components.filter(
+        (component: Component) =>
+          component.projectName === selectedProject.value && component.isActive
+      );
+    };
 
     return {
-      activeRoutes,
+      selectedProject,
+      projects,
+      activeComponents,
+      updateActiveComponents,
     };
   },
 });
