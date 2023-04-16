4ce3bde5b966cb9966b030d5cacfc429e0ebfa20
silasfelinus <silasfelinus@gmail.com>
2023-04-15 18:10:44 -0700

    we have a very good framework here

--- src/components/gamescreens/SplashImage.vue
+++ src/components/gamescreens/SplashImage.vue
@@ -1,35 +1,40 @@
 <template>
-  <div class="splash-page">
-    <img :src="randomImage" alt="Splash Image" />
+  <div class="splash-image">
+    <img :src="randomImageUrl" alt="Splash image" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref } from 'vue';
+import { defineComponent, ref, onMounted } from 'vue';
 
 export default defineComponent({
-  name: 'SplashImage',
   setup() {
-    const randomImage = ref('');
+    const randomImageUrl = ref('');
 
-    onMounted(async () => {
-      const response = await fetch('/src/assets/splash/splashImages.json');
-      const images = await response.json();
+    const getRandomImageUrl = async () => {
+      const images = Object.entries(
+        import.meta.glob('/src/assets/images/splash/*.webp')
+      );
+
       const randomIndex = Math.floor(Math.random() * images.length);
-      randomImage.value = `/src/assets/splash/${images[randomIndex]}`;
-    });
+      const [, importImage] = images[randomIndex]; // Replace '_' with ','
 
-    return {
-      randomImage,
+      const module = await importImage();
+      randomImageUrl.value = module.default;
     };
+
+    onMounted(getRandomImageUrl);
+
+    return { randomImageUrl };
   },
 });
 </script>
 
 <style scoped>
-.splash-page {
-  display: flex;
-  justify-content: center;
-  align-items: center;
+.splash-image img {
+  /* Add any styles for the image here */
+  width: 100%;
+  max-width: 100%;
+  height: auto;
 }
 </style>
